82c591ff9133d79c8fe9c8a4c83d45ce5682f898
theopenhighway <earleen.525190010@stu.untar.ac.id>
2024-10-20 15:41:45 +0700

    feat: slider js but progress color still orange

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const sliderEl = document.querySelector("#range")
+const sliderEl = document.querySelector("#myRange")
 const viewValue = document.querySelector(".view-count")
 const subPrice = document.querySelector(".subs-count")
 
-
-function progressScript() {
-    const sliderValue = sliderEl.value;
-    sliderEl.style.background = `linear-gradient(to right, #f50 ${sliderValue}%, #ccc ${sliderValue}%)`;
-  }
-  
-  progressScript()
+sliderEl.addEventListener("input", (event) => {
+    const tempSliderValue = event.target.value; 
+    
+    viewValue.textContent = tempSliderValue;
+    
+    const progress = (tempSliderValue / sliderEl.max) * 100;
+   
+    sliderEl.style.background = `linear-gradient(to right, #f50 ${progress}%, #ccc ${progress}%)`;
+  })
   
 // sliderEl.addEventListener("input", (event) => {
 //   const tempSliderValue = event.target.value;
