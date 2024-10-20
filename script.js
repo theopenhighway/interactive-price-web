5afae1288d4e3d8eb82a16b18a0c1dc1a2b9f6cf
theopenhighway <earleen.525190010@stu.untar.ac.id>
2024-10-20 16:02:40 +0700

    feat: pricing model added

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const sliderEl = document.querySelector("#myRange")
 const viewValue = document.querySelector(".view-count")
-const subPrice = document.querySelector(".subs-count")
+const subPrice = document.querySelector(".subs-cost")
 
 sliderEl.addEventListener("input", (event) => {
     const tempSliderValue = event.target.value; 
-    
     viewValue.textContent = tempSliderValue;
-    
+
+    const pricing = (tempSliderValue * 0.16).toFixed(2)
+
+    subPrice.textContent = pricing;
+
     const progress = (tempSliderValue / sliderEl.max) * 100;
+
+
    
-    sliderEl.style.background = `linear-gradient(to right, #a5f3eb ${progress}%, #ccc ${progress}%)`;
+    sliderEl.style.background = `linear-gradient(to right, #a5f3eb ${progress}%, #eaeefb ${progress}%)`;
   })
-  
-// sliderEl.addEventListener("input", (event) => {
-//   const tempSliderValue = event.target.value;
-//   viewValue.textContent = tempSliderValue;
-
-//   const progress = (tempSliderValue / sliderEl.max) * 100;
-
-//   sliderEl.style.background = `linear-gradient(to right, #f50 ${progress}%, #ccc ${progress}%)`;
-// })
